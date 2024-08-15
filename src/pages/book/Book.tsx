c4a4571584c313012c41ebefb167df8e908114f2
Viraj <virajmohit7@gmail.com>
2024-08-14 20:34:11 -0700

    some hover effects and responsive typography

--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { z } from 'zod';
 import { SectionDetails, ChapterDetails } from '../landing/Landing';  
+import { useImmerReducer } from 'use-immer';
 
 type ChapterDetailsType = z.infer<typeof ChapterDetails>;
 type SectionDetailsType = z.infer<typeof SectionDetails>;
@@ -9,24 +10,27 @@ type SectionDetailsType = z.infer<typeof SectionDetails>;
 function Book() {
   const location = useLocation();
   const chapters = location.state.tableOfContents;
+  const [chapterSelectionState, dispatchSelectionState] = useImmerReducer(chapterSelectionReducer, Array(chapters.length).fill(false));
 
 
   return (
     <section className="h-full" data-theme="autumn">
       
-      <div className="flex flex-col w-full max-w-7xl m-auto h-full px-2.5">
+      <div className="flex flex-col w-full max-w-7xl m-auto h-full px-2.5 md:text-3xl lg:text-4xl xl:text-5xl">
         <h1 className="text-center">Table of Contents</h1>
-        <div className="divider"></div>
+        <div className="divider my-1"></div>
 
-        <article className='prose w-full max-w-3xl mx-auto overflow-y-auto'>
+        <article className='prose w-full max-w-3xl mx-auto overflow-y-auto pr-3 text-xs md:text-base lg:text-3xl'>
           <ol>
             {chapters.map((chapter: ChapterDetailsType) =>
               <li key={chapter.number}>
-                <label className="label cursor-pointer">
-                  <span className="label-text text-wrap"><strong>{chapter.title}</strong></span>
-                  <input type="checkbox" className="checkbox checkbox-primary" />
-                </label>
-                <div className="divider my-1"></div>
+                <div className="group">
+                  <label className="label cursor-pointer">
+                    <span className="label-text text-wrap"><strong>{chapter.title}</strong></span>
+                    <input type="checkbox" className="checkbox checkbox-primary" />
+                  </label>
+                  <div className="divider my-1 group-hover:divider-primary"></div>
+                </div>
                 <ol className="my-1 list-[upper-roman]">
                   {chapter.sections.map((section: SectionDetailsType) =>
                     <li key={section.number}>
