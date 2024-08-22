ad89fd8018c0fafc7184050798abf278b9c24c4c
Viraj Bansal <viraj.bansal@alphafmc.com>
2024-08-22 02:08:11 -0700

    some very basic ui setup for contents page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Landing from './pages/landing/Landing'
-import BookContentSelector from './pages/Content Selection/ContentSelection'
+import BookContentSelector from './pages/content selection/ContentSelection'
+import Content from './pages/content/Content'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/book" element={<BookContentSelector />} />
+      <Route path="/content" element={<Content />} />
     </Routes>
     
   )
---
+++ src/pages/content/Content.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <section className="h-full" data-theme="autumn">
+          <div className="flex flex-col w-full max-w-7xl m-auto h-full px-2.5">
+            <h1 className="text-center text-5xl lg:text-6xl xl:text-7xl">
+              Reading + Problems
+            </h1>
+            <div className="divider my-1"></div>
+          </div>
+        </section>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
