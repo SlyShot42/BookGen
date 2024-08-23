15b7f56095eddb2d43e5f6ab8b06fc52295d5fe2
Viraj Bansal <viraj.bansal@alphafmc.com>
2024-08-23 02:32:24 -0700

    performed a very crude implementation of markdown rendering so far

--- src/components/BookGenerator.tsx
+++ src/components/BookGenerator.tsx
@@ -1,66 +1,97 @@
-import OpenAI from 'openai';
-import { useImmerReducer } from 'use-immer';
-import { ChapterDetails } from '../pages/landing/Landing';
-import { useEffect } from 'react';
-import { useRef } from 'react';
-import { z } from 'zod';
+import OpenAI from "openai";
+import { useImmerReducer } from "use-immer";
+import { ChapterDetails } from "../pages/landing/Landing";
+import { useEffect } from "react";
+import { useRef } from "react";
+import { z } from "zod";
+import { useNavigate } from "react-router-dom";
 
-const openai = new OpenAI({ apiKey: import.meta.env.VITE_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
 
 type ChapterDetailsType = z.infer<typeof ChapterDetails>;
 
-function BookGenerator({chapters, topic, sectionSelections = null}: {chapters: ChapterDetailsType[] | null, topic: string, sectionSelections?: boolean[][] | null}) {
-  
-  
-  const totalSections = chapters!.reduce((accumulator, currentValue) => accumulator + currentValue.sections.length, 0);
-  
-  
+function BookGenerator({
+  chapters,
+  topic,
+  sectionSelections = null,
+}: {
+  chapters: ChapterDetailsType[] | null;
+  topic: string;
+  sectionSelections?: boolean[][] | null;
+}) {
+  const navigate = useNavigate();
+
+  const totalSections = chapters!.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.sections.length,
+    0
+  );
+
   // compute max progress by totalling the trues in sectionSelections or using totalSections
-  const maxProgress = sectionSelections ? sectionSelections.flat().reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0) : totalSections;
+  const maxProgress = sectionSelections
+    ? sectionSelections
+        .flat()
+        .reduce(
+          (accumulator, currentValue) => accumulator + Number(currentValue),
+          0
+        )
+    : totalSections;
 
-  const [currentProgress, dispatchProgress] = useImmerReducer(progressReducer, 0);
+  const [currentProgress, dispatchProgress] = useImmerReducer(
+    progressReducer,
+    0
+  );
 
-  const sectionContent = useRef(chapters!.map((chapter: ChapterDetailsType) => Array(chapter.sections.length).fill('')));
+  const sectionContent = useRef(
+    chapters!.map((chapter: ChapterDetailsType) =>
+      Array(chapter.sections.length).fill("")
+    )
+  );
   const currentIndex = useRef([0, 0]);
-  
+
   useEffect(() => {
-      const generateContent = async (title: string) => {
-        let content: string | null = "";
-        try {
-          const completion = await openai.chat.completions.create({
-            model: "gpt-4o-2024-08-06",
-            messages: [
-              {
-                  "role": "system",
-                  "content": "You are a course textbook content generation machine designed to output in markdown(surround inline latex with $..$ and display latex with $$..$$). Do not acknowledge or greet. Output the content only. Expand on information where appropriate.",
-              },
-              {
-                  "role": "user",
-                  "content": "Generate the content of the section (do not include section title in reponse): \n"
-                  + title
-                  + "\n in the\n"
-                  + topic
-                  + "\n textbook.",
-              },
-            ],
-            temperature: 0.4
-          });
-          console.log(completion.choices[0].message.content)
-          content = completion.choices[0].message.content;
-        } catch(error) {
-          console.error("Error fetching completion", error);
-        } finally {
-          dispatchProgress({ type: 'increment' });
-        }
-        return content;
+    const generateContent = async (title: string) => {
+      let content: string | null = "";
+      try {
+        const completion = await openai.chat.completions.create({
+          model: "gpt-4o-2024-08-06",
+          messages: [
+            {
+              role: "system",
+              content:
+                "You are a course textbook content generation machine designed to output in markdown(surround inline latex with $..$ and display latex with $$..$$). Do not acknowledge or greet. Output the content only. Expand on information where appropriate.",
+            },
+            {
+              role: "user",
+              content:
+                "Generate the content of the section (do not include section title in reponse): \n" +
+                title +
+                "\n in the\n" +
+                topic +
+                "\n textbook.",
+            },
+          ],
+          temperature: 0.4,
+        });
+        console.log(completion.choices[0].message.content);
+        content = completion.choices[0].message.content;
+      } catch (error) {
+        console.error("Error fetching completion", error);
+      } finally {
+        dispatchProgress({ type: "increment" });
       }
-      
+      return content;
+    };
+
     if (currentProgress < maxProgress) {
       if (sectionSelections) {
         const [chapterIndex, sectionIndex] = currentIndex.current;
         if (sectionSelections[chapterIndex][sectionIndex]) {
-          sectionContent.current[chapterIndex][sectionIndex] = generateContent(chapters![chapterIndex].sections[sectionIndex].title);
-          
+          sectionContent.current[chapterIndex][sectionIndex] = generateContent(
+            chapters![chapterIndex].sections[sectionIndex].title
+          );
         }
 
         if (sectionIndex < sectionSelections[chapterIndex].length - 1) {
@@ -70,7 +101,9 @@ function BookGenerator({chapters, topic, sectionSelections = null}: {chapters: C
         }
       } else {
         const [chapterIndex, sectionIndex] = currentIndex.current;
-        sectionContent.current[chapterIndex][sectionIndex] = generateContent(chapters![chapterIndex].sections[sectionIndex].title);
+        sectionContent.current[chapterIndex][sectionIndex] = generateContent(
+          chapters![chapterIndex].sections[sectionIndex].title
+        );
 
         if (sectionIndex < chapters![chapterIndex].sections.length - 1) {
           currentIndex.current = [chapterIndex, sectionIndex + 1];
@@ -78,30 +111,43 @@ function BookGenerator({chapters, topic, sectionSelections = null}: {chapters: C
           currentIndex.current = [chapterIndex + 1, 0];
         }
       }
+    } else {
+      navigate("/content", {
+        state: { chapters, sectionContent: sectionContent.current },
+      });
     }
-  }, [chapters, currentProgress, dispatchProgress, maxProgress, sectionSelections, topic])
-
+  }, [
+    chapters,
+    currentProgress,
+    dispatchProgress,
+    maxProgress,
+    sectionSelections,
+    topic,
+    navigate,
+  ]);
 
   return (
     <>
-        <h1>Generating Book</h1>
-        <progress 
-          className="progress progress-primary w-full" 
-          value={currentProgress}
-          max={maxProgress}
-        />
-        <p>{currentProgress}/{maxProgress}</p>
+      <h1>Generating Book</h1>
+      <progress
+        className="progress progress-primary w-full"
+        value={currentProgress}
+        max={maxProgress}
+      />
+      <p>
+        {currentProgress}/{maxProgress}
+      </p>
     </>
-  )
+  );
 }
 
 const progressReducer = (draft: number, action: { type: string }) => {
   switch (action.type) {
-    case 'increment':
+    case "increment":
       return draft + 1;
     default:
       return;
   }
-}
+};
 
-export default BookGenerator
+export default BookGenerator;
--- src/pages/content/Content.tsx
+++ src/pages/content/Content.tsx
@@ -1,22 +1,83 @@
+import Markdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import "katex/dist/katex.min.css";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const markdown = `Here is some ruby code:
+
+~~~python
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("World")
+~~~
+`;
+
 function Content() {
+  // const markdown = `
+  // # Sample Markdown
+  // \`\`\`javascript
+  // console.log('Hello, world!');
+  // \`\`\`
+  // `;
   return (
-    <div className="drawer lg:drawer-open">
+    <div className="drawer h-full lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        {/* <section className="h-full" data-theme="autumn">
+      <div className="drawer-content h-full">
+        <section className="h-full" data-theme="autumn">
           <div className="flex flex-col w-full max-w-7xl m-auto h-full px-2.5">
-            <h1 className="text-center text-5xl lg:text-6xl xl:text-7xl">
+            <label
+              htmlFor="my-drawer-2"
+              className="btn btn-circle btn-ghost drawer-button lg:hidden mt-2"
+            >
+              <svg
+                className="swap-off fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 512 512"
+              >
+                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+              </svg>
+            </label>
+            <h1 className="text-center text-4xl lg:text-5xl xl:text-7xl">
               Reading + Problems
             </h1>
             <div className="divider my-1"></div>
+
+            <div className="overflow-y-auto">
+              <article className="prose w-full max-w-3xl mx-auto">
+                <Markdown
+                  remarkPlugins={[remarkMath]}
+                  rehypePlugins={[rehypeKatex]}
+                  children={markdown}
+                  components={{
+                    code(props) {
+                      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                      const { children, className, node, ref, ...rest } = props;
+                      const match = /language-(\w+)/.exec(className || "");
+                      return match ? (
+                        <SyntaxHighlighter
+                          {...rest}
+                          PreTag="div"
+                          children={String(children).replace(/\n$/, "")}
+                          language={match[1]}
+                          style={oneDark}
+                        />
+                      ) : (
+                        <code {...rest} className={className}>
+                          {children}
+                        </code>
+                      );
+                    },
+                  }}
+                />
+              </article>
+            </div>
           </div>
-        </section> */}
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
+        </section>
       </div>
       <div className="drawer-side">
         <label
